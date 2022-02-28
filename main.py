e8ddd85809a08ef727d9866919f4ae2cead91dfd
E-leg <emmanuel.leguet@hotmail.fr>
2022-02-28 23:40:40 +0100

    growth_stage function

--- main.py
+++ main.py
@@ -93,24 +93,66 @@ def image2graph(img, O, B, nbins=10):
 
     return G, A
 
-def growth_stage(A,S,T):
-    global P
-    return 0 # path
+
+def growth_stage(G, A):
+    """Expand the trees S and T. The active nodes A explore adjacent non-saturated
+    edges and acquire new children from a set of free nodes. The newly acquired nodes become
+    active members of the corresponding search trees. As soon as all neighbors of a given active
+    node are explored the active node becomes passive. The growth stage terminates if an active
+    node encounters a neighboring node that belongs to the opposite tree. In this case we detect a
+    path from the source to the sink."""
+
+    def get_path(node1, node2):
+        """Construct the path from the graph G and the 
+        touching nodes from trees S and T."""
+        # Recover parents recursively
+        parents1, parents2 = [node1], [node2]
+        while G.nodes[parents1[-1]]['parent']:
+            parents1.append(G.nodes[parents1[-1]]['parent'])
+        while G.nodes[parents2[-1]]['parent']:
+            parents2.append(G.nodes[parents2[-1]]['parent'])
+
+        # Create the path from the source S to the sink T
+        if G.nodes[node1]['tree'] == 'S':
+            return parents1[::-1] + parents2
+        else:
+            return parents2[::-1] + parents1
+    
+    while A:
+        active = A[0]
+        print(A)
+        for neigh in G.neighbors(active):
+            if neigh not in ['S', 'T']:
+                edge = G.get_edge_data(active, neigh)
+
+                if edge['capacity'] < edge['flow']:
+                    pass # Saturated egde
+
+                if G.nodes[neigh]['tree'] == None:
+                    # Add it to the growing tree (S or T)
+                    G.nodes[neigh]['tree'] = G.nodes[active]['tree']
+                    # Set the active node as its parent
+                    G.nodes[neigh]['parent'] = active
+                    # Set it as active
+                    A.append(neigh)
+
+                elif  G.nodes[neigh]['tree'] != G.nodes[active]['tree']:
+                    return get_path(G, active, neigh)
+        A.pop(0)
+    return []
 
 def augment(S,T):
-    global P
     pass
 
 def adopt(O):
     pass
 
 def segment():
-    global P
     # initialize: S = {s}, T = {t}, A = {s, t}, O = ∅
     while True:
         # grow S or T to find an augmenting path P from s to t
-        P_isempty=growth_stage()
-        if P_isempty:
+        P = growth_stage()
+        if not P:
             break
         # if P = ∅ terminate
         # augment on P
