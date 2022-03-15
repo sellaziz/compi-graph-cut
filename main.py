8e9bda80ee6850714535cfa5b02765e922caa533
E-leg <emmanuel.leguet@hotmail.fr>
2022-03-15 10:01:04 +0100

    Update growth & augment

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def image2graph(img, O, B, nbins=10):
         G.nodes[node]['tree'] = 'T'
 
     # Initial active nodes
-    A = O + B
+    A = ['S','T']
 
     return G, A
 
@@ -152,20 +152,32 @@ def growth_stage(G, A):
     return []
 
 def augment(G, P):
+    # Initialize orphans
     Orphans = []
+
+    # Retrieve capacities, flows & residual capacities
     capacities = np.array([G.get_edge_data(P[i], P[i+1])['capacity'] for i in range(len(P)-1)])
     flows = np.array([G.get_edge_data(P[i], P[i+1])['flow'] for i in range(len(P)-1)])
     residuals = capacities - flows
+
+    # Find the bottleneck
     df = np.min(residuals)
+
+    # Compute new flows & residuals
     new_flows = flows + df
     new_residuals = residuals - df
-
+    
+    # Update graph
     for i in range(len(P)-1):
+        # Update flow
         G[P[i]][P[i+1]]['flow'] = new_flows[i]
+
+        # Saturated node
         if new_residuals[i] == 0:
             if G.nodes[P[i]]['tree'] == 'S' and G.nodes[P[i+1]]['tree'] == 'S':
                 G.nodes[P[i+1]]['parent'] == None
                 Orphans.append(P[i+1])
+                
             if G.nodes[P[i]]['tree'] == 'T' and G.nodes[P[i+1]]['tree'] == 'T':
                 G.nodes[P[i]]['parent'] == None
                 Orphans.append(P[i])
