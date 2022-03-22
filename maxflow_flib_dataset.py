82fcb16925bd64021166e29747653c30a0b01c93
sellaziz <59337531+sellaziz@users.noreply.github.com>
2022-03-22 23:04:28 +0100

    update with new functions

--- maxflow_flib_dataset.py
+++ maxflow_flib_dataset.py
@@ -5,27 +5,29 @@ from matplotlib import pyplot as ppl
 from skimage.color import rgb2gray
 import maxflow
 from examples_utils import plot_graph_2d
-from Img2Graph import capacity, Rp, dist, Bpq, prior_probs, initialize_priors 
+from Img2Graph import  image2graph_lib, initialize_priors
 from skimage.transform import rescale, resize, downscale_local_mean
 
 
 ### Resized Horse
-# img = np.array(rgb2gray(imread("images/dog.jpg")))
-# img_painted = np.array(imread("images/dog_ST.jpg"))
-img = np.array(rgb2gray(imread("dataset/images/227092.jpg", as_gray=False, pilmode="RGB")))
-img_painted = np.array(imread("dataset/images-labels/227092-anno.png", as_gray=False, pilmode="RGB"))
+img = np.array(rgb2gray(imread("images/dog_small.jpg")))
+img_painted = np.array(imread("images/dog_small_ST.jpg"))
+# img = np.array(rgb2gray(imread("dataset/images/227092.jpg", as_gray=False, pilmode="RGB")))
+# img_painted = np.array(imread("dataset/images-labels/227092-anno.png", as_gray=False, pilmode="RGB"))
 # print(img_painted.shape)
 # img = resize(img, (img.shape[0] // 4, img.shape[1] // 4),
 #                        anti_aliasing=True)
-source,sink=initialize_priors(img_painted)
+# source,sink=initialize_priors(img_painted)
 
 
 # ### Resized Horse
 # img = rgb2gray(imread("images/horse.jpg"))
 # # img = resize(img, (img.shape[0] // 4, img.shape[1] // 4),
 # #                        anti_aliasing=True)
-# source,sink=[(img.shape[0]//2,img.shape[1]//2)],[(2,2)]
+# img_painted = np.array(imread("images/horse_ST.jpg", as_gray=False, pilmode="RGB"))
 
+# source,sink=[(img.shape[0]//2,img.shape[1]//2), (img.shape[0]//2+1,img.shape[1]//2+1)],[(2,2),(2+1,2+1)]
+source,sink=initialize_priors(img_painted)
 ### Image with squares
 # img = np.zeros((10,10))
 # img[:4,:4] = 1
@@ -36,42 +38,8 @@ source,sink=initialize_priors(img_painted)
 ppl.imshow(img, cmap='gray')
 ppl.show()
 
-def image2graph(img, O, B, nbins=10, alpha=10):
-    """Convert the input image into a graph for the segmentation part.
-        O, B: Object and Background pixels as list of tuple.
-    """
-    n,p = img.shape
-    probs = prior_probs(img[tuple(np.array(O).T)], img[tuple(np.array(B).T)], nbins, alpha)
-
-    # Initialize graph without S and T nodes
-    G = maxflow.Graph[float](0, 0)
-    nodeids = G.add_grid_nodes(img.shape)
-    for i in range(n):
-        for j in range(p):
-            if i+1 < n:
-                G.add_edge(nodeids[i,j],nodeids[i+1,j], capacity(img,(i,j),(i+1,j), O,B,probs),capacity(img,(i,j),(i+1,j), O,B, probs))
-            if j+1 < p:
-                G.add_edge(nodeids[i,j],nodeids[i,j+1], capacity(img,(i,j),(i,j+1), O,B,probs),capacity(img,(i,j),(i,j+1), O,B, probs))
-    
-    # Compute K
-    K = 0
-    Gnx=G.get_nx_graph()
-    for x in Gnx.nodes:
-        K = max(K, np.sum([Gnx.get_edge_data(x,y)['weight'] for y in Gnx.neighbors(x)]))
-    K += 1
-    print(f"K = {K}")
-
-    # Add edges for S and T
-    for i in range(n):
-        for j in range(p):
-            G.add_tedge(nodeids[i,j], capacity(img,(i,j),'S',O,B,probs, K=K),capacity(img,(i,j),'T',O,B,probs,K=K))
-    
-    # # Initial active nodes
-    print('graph init done')
-    # plot_graph_2d(G, nodeids.shape, plot_terminals=True)
-    return G, nodeids
-
-G, nodeids=image2graph(img, source,sink, nbins=10)
+G, Rp, nodeids=image2graph_lib(img, source,sink,prior_as_index=True, nbins=10, σ=0.1, λ=0.1)
+# plot_graph_2d(G, nodeids.shape, plot_terminals=True)
 G.maxflow()
 sgm = G.get_grid_segments(nodeids)
 print(sgm)
